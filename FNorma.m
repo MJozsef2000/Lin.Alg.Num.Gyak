2723b3d70d1474eb14bec4baf2088b85a9e92bf7
MJozsef2000 <78852131+MJozsef2000@users.noreply.github.com>
2021-10-03 21:02:45 +0200

    2021/10/3

---
+++ FNorma.m
@@ -0,0 +1,8 @@
+M = input('Kérem adja meg a mátrixot!\n');
+sum = 0;
+for i = 1 : size(M,1)
+    for j = 1 : size(M,2)
+        sum = sum + abs(M(i,j))^2;
+    end
+end
+fprintf('A Frobenius-norma értéke: %d', sqrt(sum));
---
+++ SorOsszegNorma.m
@@ -0,0 +1,12 @@
+M = input('Kérem adja meg a mátrixot!\n');
+max = 0;
+for i = 1 : size(M,1)
+    temp = 0;
+    for j = 1 : size(M,2)
+        temp = temp + abs(M(i,j));
+    end
+    if max < temp
+        max = temp;
+    end
+end
+fprintf('Ez a sorosszeg norma: %d\n', max);
